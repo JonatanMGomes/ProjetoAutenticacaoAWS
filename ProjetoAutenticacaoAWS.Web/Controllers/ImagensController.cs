21bc1f50713479885fe4d9bf5904d3abfa3d386c
Jonatan M Gomes <jonatan.mim@hotmail.com>
2022-07-06 23:03:28 -0300

    Criando APIs asincronas de post e delete de imagem no S3

--- ProjetoAutenticacaoAWS.Web/Controllers/ImagensController.cs
+++ ProjetoAutenticacaoAWS.Web/Controllers/ImagensController.cs
@@ -1,4 +1,5 @@
 using Amazon.S3;
+using Amazon.S3.Model;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ProjetoAutenticacaoAWS.Web.Controllers
@@ -8,6 +9,8 @@ namespace ProjetoAutenticacaoAWS.Web.Controllers
     public class ImagensController : ControllerBase
     {
         private readonly IAmazonS3 _amazonS3;
+        private static readonly List<string> _extensoesImagem =
+        new List<string>() { "image/jpeg", "image/png" };
         public ImagensController(IAmazonS3 amazonS3)
         {
             _amazonS3 = amazonS3;
@@ -27,9 +30,30 @@ namespace ProjetoAutenticacaoAWS.Web.Controllers
             return Ok(resposta);
         }
         [HttpPost()]
-        public async Task<IActionResult> CriarImagem()
+        public async Task<IActionResult> CriarImagem(IFormFile imagem)
         {
-            
+            if (!_extensoesImagem.Contains(imagem.ContentType))
+            {
+                return BadRequest("Tipo inválido!");
+            }
+            using (var streamDaImagem = new MemoryStream())
+            {
+                await imagem.CopyToAsync(streamDaImagem);
+                var request = new PutObjectRequest();
+                request.Key = "recFacial "+imagem.FileName;
+                request.BucketName = "imagens-aulas";
+                request.InputStream = streamDaImagem;
+
+                var resposta = await _amazonS3.PutObjectAsync(request);
+                return Ok();
+            }
         }
+        [HttpDelete()]
+        public async Task<IActionResult> DeletarImagem(string nomeDoArquivoNoS3)
+        {
+            var resposta = await _amazonS3.DeleteObjectAsync("imagens-aulas", nomeDoArquivoNoS3);
+            return Ok(resposta);
+        }
+
     }
 }
