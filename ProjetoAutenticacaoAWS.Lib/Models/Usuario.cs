09f8cc72351dc64468e23aaad34e9a4628031ae2
Jonatan M Gomes <jonatan.mim@hotmail.com>
2022-07-07 22:20:51 -0300

    Criando algumas validações para classe Usuario, criando UsuarioDTO e criando exception personalizada.

--- ProjetoAutenticacaoAWS.Lib/Models/Usuario.cs
+++ ProjetoAutenticacaoAWS.Lib/Models/Usuario.cs
@@ -1,3 +1,5 @@
+using ProjetoAutenticacaoAWS.Lib.MyExceptions;
+
 namespace ProjetoAutenticacaoAWS.Lib.Models
 {
     public class Usuario
@@ -12,36 +14,81 @@ namespace ProjetoAutenticacaoAWS.Lib.Models
         public DateTime DataCriacao { get; private set; }
         protected Usuario()
         {
-            
+
         }
-        public Usuario(int iD, string email, string cpf, DateTime dataNascimento, string nome, string senha, string urlImagemCadastro,
-                       DateTime dataCadastro)
+        public Usuario(int id, string email, string cpf, DateTime dataNascimento, string nome, string senha, DateTime dataCriacao)
         {
-            
+            SetId(id);
+            SetEmail(email);
+            SetCpf(cpf);
+            SetDataNascimento(dataNascimento);
+            SetNome(nome);
+            SetSenha(senha);
+            SetDataCriacao(dataCriacao);
         }
-        public void SetId(int id){
+        public void SetId(int id)
+        {
             Id = id;
         }
-        public void SetEmail(string email){
-            Email = email;
+        public void SetEmail(string email)
+        {
+            Email = ValidarEmail(email);
         }
-        public void SetCpf(string cpf){
-            Cpf = cpf;
+        public void SetCpf(string cpf)
+        {
+            Cpf = ValidarCpf(cpf);
         }
-        public void SetDataNascimento(DateTime dataNascimento){
-            DataNascimento = dataNascimento;
+        public void SetDataNascimento(DateTime dataNascimento)
+        {
+            DataNascimento = ValidarAnoNascimento(dataNascimento);
         }
-        public void SetNome(string nome){
+        public void SetNome(string nome)
+        {
             Nome = nome;
         }
-        public void SetSenha(string senha){
-            Senha = senha;
+        public void SetSenha(string senha)
+        {
+            Senha = ValidarSenha(senha);
         }
-        public void SetUrlImagemCadastro(string urlImagemCadastro){
+        public void SetUrlImagemCadastro(string urlImagemCadastro)
+        {
             UrlImagemCadastro = urlImagemCadastro;
         }
-        public void SetDataCriacao(DateTime dataCriacao){
+        public void SetDataCriacao(DateTime dataCriacao)
+        {
             DataCriacao = dataCriacao;
+        }
+        public DateTime ValidarAnoNascimento(DateTime data)
+        {
+            if (data.Year < 2010)
+            {
+                return data;
+            }
+            throw new DadosInvalidosException("Ano de nascimento deve ser menor que 2010!");
+        }
+        public string ValidarEmail(string email)
+        {
+            if (email.Contains("@"))
+            {
+                return email;
+            }
+            throw new DadosInvalidosException("Email deve conter @!");
+        }
+        public string ValidarCpf(string cpf)
+        {
+            if (cpf.Length == 11 && cpf.All(char.IsNumber))
+            {
+                return cpf;
+            }
+            throw new DadosInvalidosException("CPF deve ter 11 digitos, apenas números!");
+        }
+        public string ValidarSenha(string senha)
+        {
+            if (senha.Length >= 8)
+            {
+                return senha;
+            }
+            throw new DadosInvalidosException("Senha precisa ter no mínimo 8 caracteres!");
         }
     }
 }
---
+++ ProjetoAutenticacaoAWS.Lib/MyExceptions/DadosInvalidosException.cs
@@ -0,0 +1,10 @@
+namespace ProjetoAutenticacaoAWS.Lib.MyExceptions
+{
+    public class DadosInvalidosException : Exception
+    {
+        public DadosInvalidosException(string msg) : base(msg)
+        {
+
+        }
+    }
+}
