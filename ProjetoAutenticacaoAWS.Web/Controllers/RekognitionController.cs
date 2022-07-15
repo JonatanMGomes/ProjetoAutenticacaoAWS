eb8b57d77f44a47fc1395c9226858be4fcaff567
Jonatan M Gomes <jonatan.mim@hotmail.com>
2022-07-14 21:33:04 -0300

    Criando API de comparação facial.

--- ProjetoAutenticacaoAWS.Web/Controllers/RekognitionController.cs
+++ ProjetoAutenticacaoAWS.Web/Controllers/RekognitionController.cs
@@ -13,6 +13,35 @@ namespace ProjetoAutenticacaoAWS.Web.Controllers
         {
             _rekognitionClient = rekognitionClient;
         }
+        [HttpPost("comparar")] 
+        public async Task<IActionResult> CompararRosto(string nomeArquivoS3, IFormFile fotoLogin)
+        {
+            using (var memStream = new MemoryStream())
+            {
+                var request = new CompareFacesRequest();
+
+                var requestSource = new Image()
+                {
+                    S3Object = new S3Object()
+                    {
+                        Bucket = "imagens-aulas",
+                        Name = nomeArquivoS3
+                    }
+                };
+
+                await fotoLogin.CopyToAsync(memStream);
+                var requestTarget = new Image()
+                {
+                    Bytes = memStream
+                };
+
+                request.SourceImage = requestSource;
+                request.TargetImage = requestTarget;
+
+                var response = await _rekognitionClient.CompareFacesAsync(request);
+                return Ok(response);
+            }
+        }
 
         [HttpGet]
         public async Task<IActionResult> AnalisarRosto(string nomeArquivo)
@@ -20,15 +49,16 @@ namespace ProjetoAutenticacaoAWS.Web.Controllers
             var request = new DetectFacesRequest();
             var imagem = new Image();
 
-            var s3Object = new S3Object(){
+            var s3Object = new S3Object()
+            {
                 Bucket = "imagens-aulas",
                 Name = nomeArquivo
-                
+
             };
 
-            imagem.S3Object =  s3Object;
-            request.Image = imagem;        
-            request.Attributes = new List<string>(){"ALL"};
+            imagem.S3Object = s3Object;
+            request.Image = imagem;
+            request.Attributes = new List<string>() { "ALL" };
 
             var response = await _rekognitionClient.DetectFacesAsync(request);
 
