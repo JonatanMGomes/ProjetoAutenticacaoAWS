31f740026525207a2f0dbe6884be6223d4ab5920
Jonatan M Gomes <jonatan.mim@hotmail.com>
2022-07-18 22:22:32 -0300

    Criando métodos SalvarNoS3 e ValidarImagem para controller de usuario. Criando API CadastrarImagem na controller de usuario. Criando método de alteração de UrlImagemCadastro na interface de usuario.

--- ProjetoAutenticacaoAWS.Lib/Data/Repositorios/Interfaces/IUsuarioRepositorio.cs
+++ ProjetoAutenticacaoAWS.Lib/Data/Repositorios/Interfaces/IUsuarioRepositorio.cs
@@ -5,5 +5,6 @@ namespace ProjetoAutenticacaoAWS.Lib.Data.Repositorios.Interfaces
     public interface IUsuarioRepositorio : IRepositorioBase<Usuario>
     {
          Task AtualizarEmail(int id, string emailAtualizado);
+         Task AtualizarUrlImagemCadastro(int id, string urlAtualizada);
     }
 }
--- ProjetoAutenticacaoAWS.Web/Controllers/UsuarioController.cs
+++ ProjetoAutenticacaoAWS.Web/Controllers/UsuarioController.cs
@@ -1,3 +1,7 @@
+using Amazon.Rekognition;
+using Amazon.Rekognition.Model;
+using Amazon.S3;
+using Amazon.S3.Model;
 using Microsoft.AspNetCore.Mvc;
 using ProjetoAutenticacaoAWS.Lib.Data.Repositorios.Interfaces;
 using ProjetoAutenticacaoAWS.Lib.Models;
@@ -11,9 +15,15 @@ namespace ProjetoAutenticacaoAWS.Web.Controllers
     public class UsuarioController : ControllerBase
     {
         private readonly IUsuarioRepositorio _repositorio;
-        public UsuarioController(IUsuarioRepositorio repositorio)
+        private readonly IAmazonS3 _amazonS3;
+        private static readonly List<string> _extensoesImagem =
+        new List<string>() { "image/jpeg", "image/png" };
+        private readonly AmazonRekognitionClient _rekognitionClient;
+        public UsuarioController(IUsuarioRepositorio repositorio, IAmazonS3 amazonS3, AmazonRekognitionClient rekognitionClient)
         {
             _repositorio = repositorio;
+            _amazonS3 = amazonS3;
+            _rekognitionClient = rekognitionClient;
         }
 
         [HttpPost()]
@@ -30,6 +40,64 @@ namespace ProjetoAutenticacaoAWS.Web.Controllers
             {
                 return BadRequest(ex.Message);
             }
+        }
+        [HttpPost("imagem")]
+        public async Task<IActionResult> CadastrarImagem(int id, IFormFile imagem)
+        {
+            var nomeArquivo = await SalvarNoS3(imagem);
+            var imagemValida = await ValidarImagem(nomeArquivo);
+            if (imagemValida)
+            {
+                await _repositorio.AtualizarUrlImagemCadastro(id, nomeArquivo);
+                return Ok();
+            }
+            else
+            {
+                var response = await _amazonS3.DeleteObjectAsync("imagens-aulas", nomeArquivo);
+                return BadRequest("Imagem inválida!");
+            }
+        }
+        private async Task<string> SalvarNoS3(IFormFile imagem)
+        {
+            if (!_extensoesImagem.Contains(imagem.ContentType))
+            {
+                throw new Exception("tipo inválido!");
+            }
+            using (var streamDaImagem = new MemoryStream())
+            {
+                await imagem.CopyToAsync(streamDaImagem);
+                var request = new PutObjectRequest();
+                request.Key = "recFacial " + imagem.FileName;
+                request.BucketName = "imagens-aulas";
+                request.InputStream = streamDaImagem;
+
+                var resposta = await _amazonS3.PutObjectAsync(request);
+                return request.Key;
+            }
+        }
+        private async Task<bool> ValidarImagem(string nomeArquivo)
+        {
+            var request = new DetectFacesRequest();
+            var imagem = new Image();
+
+            var s3Object = new Amazon.Rekognition.Model.S3Object()
+            {
+                Bucket = "imagens-aulas",
+                Name = nomeArquivo
+
+            };
+
+            imagem.S3Object = s3Object;
+            request.Image = imagem;
+            request.Attributes = new List<string>() { "ALL" };
+
+            var response = await _rekognitionClient.DetectFacesAsync(request);
+
+            if (response.FaceDetails.Count == 1 && response.FaceDetails.First().Eyeglasses.Value == false)
+            {
+                return true;
+            }
+            return false;
         }
         [HttpGet()]
         public async Task<IActionResult> BuscarUsuarios()
